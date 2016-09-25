ebe85be5bdf4c968f1f485b34acec546ac32f75e
wenweixu <xww0701@gmail.com>
2016-09-25 13:17:11 -0700

    Add LinkedList Definition

---
+++ CodeInterview/LinkedList/LinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class Node<T>
+    {
+        Node<T> next = null;
+        T data;
+
+        public Node (T d)
+        {
+            this.data = d;
+        }
+
+        public void appendToTail(T d)
+        {
+            Node<T> end = new Node<T>(d);
+            Node<T> n = this;
+
+            while (n.next != null) {
+                n = n.next;
+            }
+
+            n.next = end;
+        }
+    }
+}
+  
---
+++ CodeInterview/LinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> n = new Node<int>(1);
+            n.appendToTail(2);
+            n.appendToTail(3);
+            n.appendToTail(4);
+
+
+       
+        }
+    }
+}
